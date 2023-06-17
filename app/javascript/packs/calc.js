bd28e5f3038e6ba3d3b71fe9026e5ef068603e69
kuragekamaboko <kamaboko2020@gmail.com>
2023-06-17 22:44:11 +0900

    ActiveHashの修正、バリデーションの追加、ActiveStorageの追加

---
+++ app/javascript/packs/calc.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',()=>{
+  const inputPriceElm = document.getElementById('item-price');
+  const addTaxPriceElm = document.getElementById('add-tax-price');
+  const profitElm = document.getElementById('profit');
+  
+  inputPriceElm.addEventListener('change',(event)=>{
+    const taxPrice = Math.floor(event.target.value * 0.1);
+    addTaxPriceElm.textContent = taxPrice;
+    profitElm.textContent = event.target.value - taxPrice;
+  })
+})
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,19 @@ class Item < ApplicationRecord
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :scheduled_delivery
-  belongs_to :prefecture_id
+  belongs_to :prefecture
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :price, presence: true,numericality: { only_integer: true ,greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  validates :scheduled_delivery_id, presence: true
+
+  belongs_to :user
+  has_one_attached :image
+
 end
