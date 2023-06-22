f3b43b0b41336f2bf56bc1b61278e66cf4e7b7af
kuragekamaboko <kamaboko2020@gmail.com>
2023-06-23 00:18:02 +0900

    バリテーションもれ対応

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,56 +6,51 @@ RSpec.describe Item, type: :model do
   end
   describe '商品新規登録' do
     context '商品登録できないとき' do 
-      context '商品名' do 
         it '商品名が空では登録できない' do
           @item.name = '';
           @item.valid?
           expect(@item.errors.full_messages).to include("Name can't be blank")
         end
-      end
-      context '商品の説明' do 
+        it '商品名が40文字より多いと登録できない' do
+          @item.name = 'あ'*41;
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+        end
         it '商品の説明が空では登録できない' do
           @item.info = ''
           @item.valid?
           expect(@item.errors.full_messages).to include("Info can't be blank")
         end
-      end
-      context 'カテゴリー' do 
+        it '商品の説明が1000文字より多いと登録できない' do
+          @item.info = 'あ'*1001;
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Info is too long (maximum is 1000 characters)")
+        end
         it 'カテゴリーが空では登録できない' do
           @item.category_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("Category can't be blank")
         end
-      end
-      context '商品の状態' do 
         it '商品の状態が空だと登録できない' do
           @item.sales_status_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("Sales status can't be blank")
         end
-      end
-      context '配送料の負担' do
         it '配送料の負担が空では登録できない' do
           @item.shipping_fee_status_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
         end
-      end
-      context '発送元の地域' do 
         it '発送元の地域が空では登録できない' do
           @item.prefecture_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("Prefecture can't be blank")
         end
-      end
-      context '発送までの日数' do 
         it '発送までの日数が空では登録できない' do
           @item.scheduled_delivery_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
         end
-      end
-      context '販売価格' do 
         it '販売価格が空では登録できない' do
           @item.price = nil
           @item.valid?
@@ -71,14 +66,11 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
         end
-      end
-      context '商品画像' do 
         it '商品画像が空では登録できない' do
           @item.image = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("Image can't be blank")
         end
-      end
     end
     context '新規登録ができるとき' do
       it '必要な情報が入力されれば登録できる' do
