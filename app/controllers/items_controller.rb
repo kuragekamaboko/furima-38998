370b247094290f802ddcd25e66457f9d0aae722f
kuragekamaboko <kamaboko2020@gmail.com>
2023-07-01 19:56:41 +0900

    商品取得方法のへんこう

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit,:update,:destroy]
-  before_action :find_item,only:[:show,:edit,:update]
+  before_action :find_item,only:[:show,:edit,:update,:destroy]
   def index
     @items = Item.all.order(created_at: "DESC")
   end
@@ -41,7 +41,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if @item.destroy
       redirect_to root_path
     end
