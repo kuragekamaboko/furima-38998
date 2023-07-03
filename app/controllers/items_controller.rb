c1b24e90af35133d503c8f6e66ec18c466e98787
kuragekamaboko <kamaboko2020@gmail.com>
2023-07-03 18:31:01 +0900

    指摘事項の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+
+    if @item.user.id == current_user.id && @item.destroy
       redirect_to root_path
     end
   end
